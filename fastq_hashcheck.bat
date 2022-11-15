7c30ea7d6339229b84ac13ce9edb742a226ad53c
Philip Davidson <philip.davidson2@nhs.net>
2022-11-15 12:56:34 +0000

    Win7 requires hash type to be in caps

--- fastq_hashcheck.bat
+++ fastq_hashcheck.bat
@@ -3,7 +3,7 @@
 REM Generates a hashfile for all files of a specified file extention saves to the same directory.
 REM Written by Phil Davidson to enable hashchecking on fastq files by running on sequencer
 
-set hashtype=md5
+set hashtype=MD5
 set file_type=fastq.gz
 set output_file=fastq.md5
 
